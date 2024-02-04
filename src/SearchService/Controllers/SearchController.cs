e7e1bda0cc158cf4f460360fac89e0b2bcf749ba
SteveGuoh <hghandle@outlook.com>
2024-02-04 23:07:06 +1100

    paste search controller from source code

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -5,10 +5,10 @@ namespace SearchService;
 
 [ApiController]
 [Route("api/search")]
-public class SearchController : Controller
+public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
         var query = DB.PagedSearch<Item, Item>();
 
@@ -22,14 +22,15 @@ public class SearchController : Controller
             "make" => query.Sort(x => x.Ascending(a => a.Make))
                 .Sort(x => x.Ascending(a => a.Model)),
             "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
-            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
         };
 
         query = searchParams.FilterBy switch
         {
             "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
-            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
         };
 
         if (!string.IsNullOrEmpty(searchParams.Seller))
@@ -42,7 +43,8 @@ public class SearchController : Controller
             query.Match(x => x.Winner == searchParams.Winner);
         }
 
-        query.PageNumber(searchParams.PageNumber).PageSize(searchParams.PageSize);
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
